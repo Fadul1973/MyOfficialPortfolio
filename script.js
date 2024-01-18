e86675d8a3a220df7f24e1e7ea3d238dd16fbab2
Fadul1973 <b8039457@my.shu.ac.uk>
2024-01-18 18:43:58 +0000

    Added About Me and Education sections!

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ toggleIcon.addEventListener("click", () => {
   toggleIcon.classList.toggle("bx-sun");
   document.body.classList.toggle("dark-mode");
 });
+
+// STICKY NAVBAR
+window.onscroll = () => {
+  const header = document.querySelector(".header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
